37320c416aa90a34233b5767130bff3af071cf22
James Kerr <jpm.kerr@outlook.com>
2017-08-24 15:10:24 +1200

    Added Database class with example in Main Should allow work on #5

---
+++ src/main/java/ac/waikato/nz/middlemanagement/Database.java
@@ -0,0 +1,56 @@
+package ac.waikato.nz.middlemanagement;
+
+import org.h2.jdbcx.JdbcConnectionPool;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+
+/**
+ * Singleton class for the H2 database
+ */
+public class Database {
+    private static Database instance;
+    private static String connectionString = "jdbc:h2:/test";
+    private static String dbUsername = "sa";
+    private static String dbPassword = "sa";
+
+    private final JdbcConnectionPool cp;
+
+    private Database(){
+        cp = JdbcConnectionPool.
+                        create(connectionString, dbUsername, dbPassword);
+    }
+
+    /**
+     *
+     * @return the instance of the database, creates it if it doesn't exist.
+     */
+    public static Database getDatabase(){
+        if(instance == null)
+            instance = new Database();
+        return instance;
+    }
+
+    /**
+     * Disposes of the connection pool
+     * Should only be used for shutdown
+     */
+
+    public static void dispose(){
+        instance.cp.dispose();
+        instance = null;
+    }
+
+    /**
+     *
+     * @return A connection from the pool. REMEMBER TO .close() AND .dispose() WHEN DONE!!!
+     * @throws SQLException
+     */
+    public Connection getConnection() throws SQLException {
+        return cp.getConnection();
+    }
+
+
+}
--- src/main/java/ac/waikato/nz/middlemanagement/Main.java
+++ src/main/java/ac/waikato/nz/middlemanagement/Main.java
@@ -1,7 +1,30 @@
 package ac.waikato.nz.middlemanagement;
 
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 public class Main{
     public static void main(String [] args){
-        System.out.println("Hello, World!");
+        try {
+            Database db = Database.getDatabase(); //Initialise the connection
+            Connection connection = db.getConnection();
+
+            final boolean execute = connection.createStatement().execute(
+                    "CREATE TABLE IF NOT EXISTS example(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255)); INSERT INTO example(name) VALUES ('foo');");
+            final ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM example;");
+            while (resultSet.next()){
+                System.out.println( resultSet.getString(1) +":"+ resultSet.getString(2));
+            }
+
+            connection.close();
+
+            Database.dispose();
+
+            //Remember: To close all database connections before we exit;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
