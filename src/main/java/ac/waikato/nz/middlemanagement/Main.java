d9eed531715b7bacdc72a64182f96c4b3d3a4138
James Kerr <lords-death@hotmail.com>
2017-09-24 22:02:24 +1300

    Added script support via CLI

--- src/main/java/ac/waikato/nz/middlemanagement/Main.java
+++ src/main/java/ac/waikato/nz/middlemanagement/Main.java
@@ -1,11 +1,45 @@
 package ac.waikato.nz.middlemanagement;
 
 
+import org.apache.commons.cli.*;
+
+import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Main{
     public static void main(String [] args){
+        CommandLineParser parser = new DefaultParser();
+        Options options = new Options();
+        HelpFormatter formatter = new HelpFormatter();
+        options.addOption("script", true, "script to execute on the database");
+        options.addOption("help", "displays this message");
+
+
+        try {
+            CommandLine cmd = parser.parse( options, args);
+            if(cmd.hasOption("help")){
+                formatter.printHelp( "WUGS", options );
+            } else if(cmd.hasOption("script")){
+                Database db = Database.getDatabase();
+                Connection connection = db.getConnection();
+                String statement = "RUNSCRIPT FROM '" + cmd.getOptionValue("script") +"';";
+                final boolean execute = connection.createStatement().execute(statement);
+                connection.close();
+                Database.dispose();
+            } else {
+                startDB();
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void startDB() {
         try {
             Database db = Database.getDatabase(); //Initialise the connection
             Connection connection = db.getConnection();
