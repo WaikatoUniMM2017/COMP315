dc7274511e717fcce9d279e5f4441f1af1903196
James Kerr <lords-death@hotmail.com>
2017-10-01 14:07:28 +1300

    Swapped tomcat for Undertow in interests of saving time Example web server

--- src/main/java/ac/waikato/nz/middlemanagement/Main.java
+++ src/main/java/ac/waikato/nz/middlemanagement/Main.java
@@ -1,9 +1,12 @@
 package ac.waikato.nz.middlemanagement;
 
 
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
 import org.apache.commons.cli.*;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,18 +21,40 @@ public class Main{
 
 
         try {
+            Database db = Database.getDatabase();
             CommandLine cmd = parser.parse( options, args);
             if(cmd.hasOption("help")){
                 formatter.printHelp( "WUGS", options );
             } else if(cmd.hasOption("script")){
-                Database db = Database.getDatabase();
+
                 Connection connection = db.getConnection();
                 String statement = "RUNSCRIPT FROM '" + cmd.getOptionValue("script") +"';";
                 final boolean execute = connection.createStatement().execute(statement);
                 connection.close();
-                Database.dispose();
+
             } else {
-                startDB();
+                Undertow server = Undertow.builder()
+                        .addHttpListener(8080, "localhost")
+                        .setHandler(new HttpHandler() {
+                            @Override
+                            public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+
+                                Connection connection = db.getConnection();
+                                final boolean execute = connection.createStatement().execute(
+                                        "CREATE TABLE IF NOT EXISTS example(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255)); INSERT INTO example(name) VALUES ('foo');");
+                                final ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM example;");
+                                String output = "";
+                                while (resultSet.next()){
+                                    output +=
+                                            resultSet.getString(1) +":"+ resultSet.getString(2) + "\r\n";
+                                }
+
+                                exchange.getResponseSender().send(output);
+                                connection.close();
+                            }
+                        }).build();
+                server.start();
             }
         } catch (ParseException e) {
             e.printStackTrace();
@@ -39,26 +64,4 @@ public class Main{
 
     }
 
-    private static void startDB() {
-        try {
-            Database db = Database.getDatabase(); //Initialise the connection
-            Connection connection = db.getConnection();
-
-            final boolean execute = connection.createStatement().execute(
-                    "CREATE TABLE IF NOT EXISTS example(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255)); INSERT INTO example(name) VALUES ('foo');");
-            final ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM example;");
-            while (resultSet.next()){
-                System.out.println( resultSet.getString(1) +":"+ resultSet.getString(2));
-            }
-
-            connection.close();
-
-            Database.dispose();
-
-            //Remember: To close all database connections before we exit;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
