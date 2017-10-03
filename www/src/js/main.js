4918fdeaaa2a319d5854db51ecac88be98b61aae
Callan <Callanhpinto1995@gmail.com>
2017-10-03 22:13:06 +1300

    last changes from yesterday and today
    Done everything except for menu links and brackets

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-	
+function check() {
+		    document.getElementById("ms1).checked = true;
+			}
+			function uncheck() {
+			    document.getElementById("ms1").checked = false;
+			}
+			document.getElementById("ms2").checked = true;
+			}
+			function uncheck() {
+			    document.getElementById("ms2").checked = false;
+			}
+			document.getElementById("ms3").checked = true;
+			}
+			function uncheck() {
+			    document.getElementById("ms3").checked = false;
+			}
+			document.getElementById("ms4").checked = true;
+			}
+			function uncheck() {
+			    document.getElementById("ms4").checked = false;
+			}
+			document.getElementById("ms5").checked = true;
+			}
+			function uncheck() {
+			    document.getElementById("ms5").checked = false;
+			}
+			
+		$( "#div.foo" ).click(function() {
+  $( "span", this ).addClass( "bar" );
+});
 })
